442007849d6807ea13f4b307ace108fd89bdfd27
sachinjachak2001 <sachinjachak10@gmail.com>
2021-06-28 19:34:38 +0530

    react app created, palette added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "./App.css";
 import seedColors from "./seedColors";
 import Palette from "./Palette";
 
 function App() {
   return (
-    <div className="App">
-      <Palette {...seedColors[4]} />
+    <div>
+      <Palette {...seedColors[2]} />
     </div>
   );
 }
