78e71e98980c025db7684227c3ed7d274f48da8a
sachinjachak2001 <sachinjachak10@gmail.com>
2021-07-21 16:19:09 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import seedColors from "./seedColors";
 import Palette from "./Palette";
 import { generatePalette } from "./ColorHelper";
 
-function App() {
-  console.log();
-
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>this is home route</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>this is palette route</h1>}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
