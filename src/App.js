d94e2e09499223987c69a346b1490f653aeb32e5
sachinjachak2001 <sachinjachak10@gmail.com>
2021-08-31 02:37:03 +0530

    Palettes saved in localstorage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import PaletteForm from "./PaletteForm";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savedPalettes || seedColors };
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
   }
@@ -20,8 +21,18 @@ class App extends Component {
     });
   }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
-    console.log(newPalette);
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+
+  syncLocalStorage() {
+    // save palette to locla storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(this.state.palettes)
+    );
   }
   render() {
     return (
