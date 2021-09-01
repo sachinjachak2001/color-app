fa69157fbdf92bf157d693f45434191e3b80ad0d
sachinjachak2001 <sachinjachak10@gmail.com>
2021-09-01 16:33:42 +0530

    make PaletteList responisive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   root: {
     backgroundColor: "blue",
@@ -13,6 +14,12 @@ export default {
     alignItems: "flex-start",
     flexDireaction: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "70%",
+    },
+    [sizes.down("lg")]: {
+      width: "80%",
+    },
   },
   nav: {
     display: "flex",
@@ -31,6 +38,13 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3,30%)",
-    gridGap: "5%",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2,50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1,100%)",
+      gridGap: "1.5rem",
+    },
   },
 };
